19b6cc20aaf86846a8dd8e0a99414e4c5fd7ebc3
DzesenKopeykin <kopeykindzzesen@gmail.com>
2018-11-24 20:29:21 +0200

    add PingBall

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
 
 
 class PongGame(Widget):
@@ -13,3 +15,14 @@ class PongApp(App):
 
 if __name__ == '__main__':
     PongApp().run()
+
+
+class PongBall(Widget):
+
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+
+    velosity = ReferenceListProperty(velosity_x, velosity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
